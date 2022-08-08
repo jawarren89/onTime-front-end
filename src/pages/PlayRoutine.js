22bddc463e838405ac92042f3f4a46ebda8d4fb3
Julie <jawarren89@gmail.com>
2022-08-07 20:59:02 -0700

    add ProgressCircle

--- src/pages/PlayRoutine.js
+++ src/pages/PlayRoutine.js
@@ -1,12 +1,17 @@
 import React from "react";
 // import PropTypes from "prop-types";
 
+import ProgressTimer from "../components/ProgressTimer";
+
 const PlayRoutine = () => {
   return (
     <>
       <main className="play-routine-container">
         <h2>Routine will play on this screen!</h2>
         <p>You can do this, I believe in you.</p>
+        <section>
+          <ProgressTimer percentage={85} colour={"teal"}></ProgressTimer>
+        </section>
       </main>
     </>
   );
