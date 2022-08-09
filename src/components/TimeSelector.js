1180dd43b297feaffb5d80fcf66d6375cb07ba16
Julie <jawarren89@gmail.com>
2022-08-09 14:15:08 -0700

    pull input variables for time dropdown elements into separate component

--- src/components/TimeSelector.js
+++ src/components/TimeSelector.js
@@ -4,13 +4,18 @@ import "../styles/TimeSelector.css";
 import React from "react";
 import PropTypes from "prop-types";
 
-import { TimeToCivilian } from "./TimeConversions";
-import { TimeToMilitary } from "./TimeConversions";
+import { TimeToCivilian, TimeToMilitary } from "./TimeConversions";
+import {
+  hoursOptions,
+  minutesOptions,
+  meridiemOptions,
+} from "./TimeDropdownData";
+
 import TimeDropdown from "./TimeDropdown";
 
 // The TimeSelector component is a 3-part dropdown to select hours, minutes,
-// and meridiem. Dropdown options are listed here and mapped into the
-// individual TimeDropdown components.
+// and meridiem. Dropdown options are listed in TimeDropdownData and mapped
+// into the individual TimeDropdown components.
 
 // Default time values shown are passed through the state selectedRoutine, which is
 // is managed in App.js. It is an object that contains the routine_id,
@@ -22,88 +27,6 @@ import TimeDropdown from "./TimeDropdown";
 // the state of the form.
 
 const TimeSelector = (props) => {
-  const hoursOptions = [
-    "--",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "11",
-    "12",
-  ];
-
-  const minutesOptions = [
-    "--",
-    "00",
-    "01",
-    "02",
-    "03",
-    "04",
-    "05",
-    "06",
-    "07",
-    "08",
-    "09",
-    "10",
-    "11",
-    "12",
-    "13",
-    "14",
-    "15",
-    "16",
-    "17",
-    "18",
-    "19",
-    "20",
-    "21",
-    "22",
-    "23",
-    "24",
-    "25",
-    "26",
-    "27",
-    "28",
-    "29",
-    "30",
-    "31",
-    "32",
-    "33",
-    "34",
-    "35",
-    "36",
-    "37",
-    "38",
-    "39",
-    "40",
-    "41",
-    "42",
-    "43",
-    "44",
-    "45",
-    "46",
-    "47",
-    "48",
-    "49",
-    "50",
-    "51",
-    "52",
-    "53",
-    "54",
-    "55",
-    "56",
-    "57",
-    "58",
-    "59",
-  ];
-
-  const meridiemOptions = ["AM", "PM"];
-
   const civCompleteTime = TimeToCivilian(props.selectedRoutine.complete_time);
 
   const onTimeChange = (event) => {
